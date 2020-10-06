1677a4a0ac09753da3c4a686e579ff20a9dcc0a6
Peter <pcurcio988@gmail.com>
2020-10-06 11:03:39 -0700

    Adding user input and variable type examples

---
+++ user_input.py
@@ -0,0 +1,8 @@
+print("Hello")
+name = input("What is your name? ")
+print("Hello " + name)
+
+number = input("What is your favorite number? ")
+#print(number * 3)
+triple = int(number) * 3
+print("Your number multiplied by 3 is " + str(triple))
